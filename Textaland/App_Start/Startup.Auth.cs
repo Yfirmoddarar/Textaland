c08ae0125b81028447f507691ae61c6c3fffd79e
jfjonsson <nonni95@gmail.com>
2014-05-07 17:13:05 +0000

    Added google login.

--- Textaland/App_Start/Startup.Auth.cs
+++ Textaland/App_Start/Startup.Auth.cs
@@ -32,7 +32,7 @@ namespace Textaland
             //   appId: "",
             //   appSecret: "");
 
-            //app.UseGoogleAuthentication();
+            app.UseGoogleAuthentication();
         }
     }
 }
