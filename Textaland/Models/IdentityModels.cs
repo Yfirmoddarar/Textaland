6b35376a268e0d838babeeda06443db73b7d7b65
jfjonsson <nonni95@gmail.com>
2014-05-07 17:22:21 +0000

    Changed ApplicationUser to UserAccount

--- Textaland/Models/IdentityModels.cs
+++ Textaland/Models/IdentityModels.cs
@@ -3,11 +3,11 @@
 namespace Textaland.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
+    public class UserAccount : IdentityUser
     {
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    public class ApplicationDbContext : IdentityDbContext<UserAccount>
     {
         public ApplicationDbContext()
             : base("DefaultConnection")
