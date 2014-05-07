db82e42227ec84309a2b2a9e2a92376407c9b8e2
jfjonsson <nonni95@gmail.com>
2014-05-07 17:04:19 +0000

    Here We Go

---
+++ Textaland/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Textaland.Startup))]
+namespace Textaland
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
