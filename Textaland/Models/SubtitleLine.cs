15d61fb6b73f52bf800a1c3d3a4065f2e33ea87d
danben4 <danielbenediktsson4@gmail.com>
2014-05-08 12:46:57 +0000

    Updated  'SubtitleLine' clas.

--- Textaland/Models/SubtitleLine.cs
+++ Textaland/Models/SubtitleLine.cs
@@ -5,7 +5,13 @@ using System.Web;
 
 namespace Textaland.Models
 {
-	public class SubtitleLine
-	{
+	public class SubtitleLine {
+		public int _id { get; set; }
+		public int _textFileId { get; set; }
+		public string _time { get; set; }
+		public string _line1 { get; set; }
+		public string _line2 { get; set; }
+
+
 	}
 }
