5a5d55f7867d0e3a64566645f4daef8f76f8249b
sindrikaldal <sindrimars@gmail.com>
2014-05-08 14:19:19 +0100

    Made some changes to the SubtitleCommentRepository

--- Textaland/DataAccessLayer/SubtitleCommentRepo.cs
+++ Textaland/DataAccessLayer/SubtitleCommentRepo.cs
@@ -6,21 +6,29 @@ using Textaland.Models;
 
 namespace Textaland.DataAccessLayer
 {
-	public class SubtitleCommentRepo
-	{
+	public class SubtitleCommentRepo {
 		private static SubtitleCommentRepo _instance;
 
-		public static SubtitleCommentRepo Instance
-		{
-			get
-			{
+		public static SubtitleCommentRepo Instance {
+			get {
 				if (_instance == null)
 					_instance = new SubtitleCommentRepo();
 				return _instance;
 			}
 		}
 
+		//initialize a list of SubtitleComments
 		private List<SubtitleComment> _subtitleComments = null;
+
+		//this operation returns all SubtitleComments
+		private IEnumerable<SubtitleComment> GetAllComments() {
+
+			//select all comments from the SubtitleComment list in an ascending order
+			var _allComments = from temp in _subtitleComments
+							   orderby temp._dateAdded ascending
+							   select temp;
+			return _allComments;						   
+		}
 
 	}
 }
