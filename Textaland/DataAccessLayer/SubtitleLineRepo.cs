c5e9c19acf165e143f2d2b3eb74922b8c36e6dc6
danben4 <danielbenediktsson4@gmail.com>
2014-05-08 14:05:53 +0000

    Updated 'SubtitleLineRepo' class.

--- Textaland/DataAccessLayer/SubtitleLineRepo.cs
+++ Textaland/DataAccessLayer/SubtitleLineRepo.cs
@@ -5,7 +5,25 @@ using System.Web;
 
 namespace Textaland.DataAccessLayer
 {
-	public class SubtitleLineRepo
-	{
+	public class SubtitleLineRepo {
+
+		// This is my repo for SubtitleLine which is private. 
+		private static SubtitleLineRepo _instance;
+
+		// My repo is private so I have to create a public repo
+		// which contains get function for my private repo.
+		public static SubtitleLineRepo Instance {
+			get {
+				// If my repo is null than I have to create a new one.
+				if (_instance == null) {
+					_instance = new SubtitleLineRepo();
+				}
+				// And than I return my repo.
+				return _instance;
+			}
+		}
+
+
+
 	}
 }
