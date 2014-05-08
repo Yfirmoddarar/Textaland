1db281c214653661b350ed36d8af1198c2a82e9e
hilmarragnars <hilmarragnars@gmail.com>
2014-05-08 23:25:43 +0100

    got rid of the intance in TranslationRequestRepo

--- Textaland/DataAccessLayer/TranslationRequestRepo.cs
+++ Textaland/DataAccessLayer/TranslationRequestRepo.cs
@@ -7,16 +7,6 @@ using Textaland.Models;
 namespace Textaland.DataAccessLayer
 {
 	public class TranslationRequestRepo {
-		private static TranslationRequestRepo _instance;
-
-		public static TranslationRequestRepo Instance {
-			get {
-				if (_instance == null) {
-					_instance = new TranslationRequestRepo();
-				}
-				return _instance;
-			}
-		}
 
 	    //Initialize a list of Translation Requests.
 		private List<TranslationRequest> _translationRequests = null;
