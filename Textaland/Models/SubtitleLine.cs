72df3d727d99f95f56178c733cece26444ebc603
danben4 <danielbenediktsson4@gmail.com>
2014-05-08 12:53:58 +0000

    Created functions for 'SubtitleLine' class.

--- Textaland/Models/SubtitleLine.cs
+++ Textaland/Models/SubtitleLine.cs
@@ -5,6 +5,7 @@ using System.Web;
 
 namespace Textaland.Models
 {
+	// This class is for every line in textfile. 
 	public class SubtitleLine {
 		public int _id { get; set; }
 		public int _textFileId { get; set; }
@@ -12,6 +13,8 @@ namespace Textaland.Models
 		public string _line1 { get; set; }
 		public string _line2 { get; set; }
 
-
+		void ShiftTime(int shiftBy);
+		void EditLine1(string newLine);
+		void EditLine2(string newLine);
 	}
 }
