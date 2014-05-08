6a6df1a2b3d0f9a54ee24af5cb1062131e516396
Jorundur <jorundurj@gmail.com>
2014-05-08 14:29:37 +0000

    Updated SubtitleFileRepo, GetAllSubtitles

--- Textaland/DataAccessLayer/SubtitleFileRepo.cs
+++ Textaland/DataAccessLayer/SubtitleFileRepo.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Textaland.Models;
 
 namespace Textaland.DataAccessLayer
 {
 	public class SubtitleFileRepo{
         private static SubtitleFileRepo _instance;
+
+        //this creates a new SubtitleFileRepo
         public static SubtitleFileRepo Instance {
             get {
                 if (_instance == null)
@@ -14,5 +17,20 @@ namespace Textaland.DataAccessLayer
                 return _instance;
             }
         }
+
+        //initialize a list of SubtitleFiles
+        private List<SubtitleFile> _subtitleFiles = null;
+
+        //this operation returns all SubtitleFiles
+        public IEnumerable<SubtitleFile> GetAllSubtitles()
+        {
+
+            //select all comments from the SubtitleComment list in an ascending order
+            var _allSubtitles = from temp in _subtitleFiles
+                               orderby temp._dateAdded ascending
+                               select temp;
+            return _allSubtitles;
+        }
+
 	}
 }
