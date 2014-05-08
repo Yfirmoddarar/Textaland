1ac8637f23bcaa4336d9e1ba15442b5d587f93a4
danben4 <danielbenediktsson4@gmail.com>
2014-05-08 13:06:14 +0000

    Created attributes for 'SubtitleFile'.

--- Textaland/Models/SubtitleFile.cs
+++ Textaland/Models/SubtitleFile.cs
@@ -5,7 +5,24 @@ using System.Web;
 
 namespace Textaland.Models
 {
-	public class SubtitleFile
-	{
+	public class SubtitleFile {
+		// Attributes.
+
+		int _id { get; set; }
+		int _userId { get; set; }
+		int _numOfTranslationParticipants { get; set; }
+		int _numOfDownloads { get; set; }
+		int _rating { get; set; }
+		string _name { get; set; }
+		string _type { get; set; }
+		string _languageFrom { get; set; }
+		string _languageTo { get; set; }
+		string _description { get; set; }
+		DateTime _dateAdded { get; set; }
+		bool _readyForDownload { get; set; }
+		bool _inTranslation { get; set; }
+		bool _hearingImpaired { get; set; }
+
+		// Operations
 	}
 }
