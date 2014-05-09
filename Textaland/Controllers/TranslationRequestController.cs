6857d0fa637107dfe39cc987ff7e5a46005453f7
sindrikaldal <sindrimars@gmail.com>
2014-05-09 14:25:36 +0100

    Added some function to TranslationRequestController

--- Textaland/Controllers/TranslationRequestController.cs
+++ Textaland/Controllers/TranslationRequestController.cs
@@ -3,16 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Textaland.Models;
 
 namespace Textaland.Controllers
 {
-    public class TranslationRequestController : Controller
-    {
-        //
-        // GET: /TranslationRequest/
-        public ActionResult Index()
-        {
-            return View();
-        }
+	public class TranslationRequestController : Controller
+	{
+		//
+		// GET: /TranslationRequest/
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		//Get
+		public ActionResult NewTranslationRequest()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult NewTranslationRequest(FormCollection formData)
+		{
+			return Index();
+		}
 	}
+
+
+	
 }
