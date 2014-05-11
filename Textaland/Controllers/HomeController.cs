0d8899783beeee220868fabafb2493fd7809e583
danben4 <danielbenediktsson4@gmail.com>
2014-05-11 16:42:28 +0000

    Some updates.

--- Textaland/Controllers/HomeController.cs
+++ Textaland/Controllers/HomeController.cs
@@ -13,20 +13,28 @@ namespace Textaland.Controllers
     {
         public ActionResult FrontPage()
         {
+			// Get the subtitles from repo.
 			SubtitleFileRepo sfp = new SubtitleFileRepo();
 
+			// Get the translation request from the repo.
+			TranslationRequest tq = new TranslationRequest();
+
+			// Here I sort the subtitles by number of downloads and add them to the
+			// ViewBag.
 			var mostPopularList = from mp in sfp.GetAllSubtitles()
 								  orderby mp._numOfDownloads descending
 								  select mp;
 
 			ViewBag.PopularList = mostPopularList;
 
+			// Here I sort the subtitles by date and add them to the ViewBag.
 			var newestList = from mp in sfp.GetAllSubtitles()
 							 orderby mp._dateAdded descending
 							 select mp;
 
 			ViewBag.NewestList = newestList;
 
+			// Here I sort the subtitles by rating and add them to the ViewBag.
 			var ratingList = from mp in sfp.GetAllSubtitles()
 								  orderby mp._rating descending
 								  select mp;
