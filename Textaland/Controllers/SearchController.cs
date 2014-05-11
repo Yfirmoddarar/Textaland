975c54486d7a9c245ff58a6130515536e1b87cf6
danben4 <danielbenediktsson4@gmail.com>
2014-05-11 12:14:48 +0000

    Created new controller and updated the front page.

---
+++ Textaland/Controllers/SearchController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Textaland.Controllers
+{
+    public class SearchController : Controller
+    {
+        //
+        // GET: /Search/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+		// Get
+		[HttpGet]
+		public ActionResult SearchResult()
+		{
+
+
+
+
+			return View();
+		}
+
+		// Post
+		[HttpPost]
+		public ActionResult SearchResult(FormCollection formData)
+		{
+
+
+
+
+			return View();
+		}
+	}
+}
