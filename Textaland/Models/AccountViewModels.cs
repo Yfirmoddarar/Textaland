42c5ebec2f79ed1929c79b595c72106b135dedb4
hilmarragnars <hilmarragnars@gmail.com>
2014-05-12 22:12:14 +0000

    translated to icelandic in the accountviews

--- Textaland/Models/AccountViewModels.cs
+++ Textaland/Models/AccountViewModels.cs
@@ -5,7 +5,7 @@ namespace Textaland.Models
     public class ExternalLoginConfirmationViewModel
     {
         [Required]
-        [Display(Name = "User name")]
+        [Display(Name = "Notandanafn:")]
         public string UserName { get; set; }
     }
 
@@ -13,51 +13,51 @@ namespace Textaland.Models
     {
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Current password")]
+        [Display(Name = "Núverandi lykilorð:")]
         public string OldPassword { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "{0} verður að innihalda að {2} eða fleiri stafi.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "New password")]
+        [Display(Name = "Nýtt lykilorð:")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        [Display(Name = "Staðfesta nýtt lykilorð:")]
+        [Compare("NewPassword", ErrorMessage = "Lykilorðið passar ekki við staðfestingu lykilorðs, reyndu aftur.")]
         public string ConfirmPassword { get; set; }
     }
 
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "User name")]
+        [Display(Name = "Notandanafn:")]
         public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Lykilorð:")]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        [Display(Name = "Muna eftir mér?")]
         public bool RememberMe { get; set; }
     }
 
     public class RegisterViewModel
     {
         [Required]
-        [Display(Name = "User name")]
+        [Display(Name = "Notandanafn:")]
         public string UserName { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "{0} verður að innihalda {2} eða fleiri stafi.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Lykilorð:")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "Staðfesta lykilorð:")]
+		[Compare("Password", ErrorMessage = "Lykilorðið passar ekki við staðfestingu lykilorðs, reyndu aftur.")]
         public string ConfirmPassword { get; set; }
     }
 }
