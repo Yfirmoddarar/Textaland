c1ac05664edb6c2387fb30333f456893791ee82b
hilmarragnars <hilmarragnars@gmail.com>
2014-05-15 17:58:52 +0000

    added Textaland.Tests

---
+++ Textaland.Tests/Controllers/TranslationRequestControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Textaland.Tests.Controllers
+{
+	[TestClass]
+	public class TranslationRequestControllerTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
